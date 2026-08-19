608db4a9dbe95343569ffe39b0c52c7bddb3c1f1
Sneha Baba <snehab.9jul@gmail.com>
2026-08-19 15:29:45 +0800

    updated standAloneTest file

--- src/test/java/SnehaBabaAutomation/Tests/StandAloneTest.java
+++ src/test/java/SnehaBabaAutomation/Tests/StandAloneTest.java
@@ -15,7 +15,7 @@ public class StandAloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//add comments//
 		String productname = "ADIDAS ORIGINAL";
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
